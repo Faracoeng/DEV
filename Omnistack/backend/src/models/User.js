b72276bfa7de8cc3012c81468649fcb1fde615c0
Faracoeng <alfmazucheli@gmail.com>
2021-02-25 11:55:52 -0300

    Entidade do User criada

---
+++ Omnistack/backend/src/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Define estrutura da entidade USER
+const UserSchema = new mongoose.Schema({
+    email: String,
+    //poderia ser também
+    // name: String,
+    // idade: Number,
+    // status: Boolean,
+});
+
+// Exportando a entidade
+module.exports = mongoose.model('User', UserSchema );
