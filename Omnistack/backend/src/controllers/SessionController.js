523cfeb2c642603fcfbd6ced95353fdf37acb17f
Faracoeng <alfmazucheli@gmail.com>
2021-02-26 10:04:05 -0300

    Email do usuario vindo pelo POST cadastrado no BCD

--- Omnistack/backend/src/controllers/SessionController.js
+++ Omnistack/backend/src/controllers/SessionController.js
@@ -5,9 +5,22 @@
 // update: Atualizar uma sessão
 // destroy: Remover/Deletar uma sessão
 
+// Para criar o usuario é necessario importar o Model User
+const User = require('../models/User');
+
 // exportar um objeto
 module.exports = {
-  store(req,res) {
-      return res.json({message: 'Hello World (Session controller)'})
+
+// Essa função pode demorar para executar, então podemos tornar ela assincrona    
+  async store(req,res) {
+      // Para cadastrar o e-mail no BCD temos que pegar o corpo da requisição
+      //Lê de tras pra frente: "Pegar o campo email do corpo (body) da requisição (req)"
+      const email = req.body.email
+    // Forma equivalente de escrever essa linha (Desestruturação)
+    // const {email} = req.body;
+    // await faz com que aguarde o usuario ser criado caso demore
+    const user = await User.create({email});
+    // Deverá retornar os dados de cadastro dentro do banco de dados
+    return res.json(user);
   } 
 };
