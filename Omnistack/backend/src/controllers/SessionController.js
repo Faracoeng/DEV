99e1c66a687701a575f92edc6e19840aa10a6392
Faracoeng <alfmazucheli@gmail.com>
2021-02-26 10:13:23 -0300

    Criando e validando usuarios (email)

--- Omnistack/backend/src/controllers/SessionController.js
+++ Omnistack/backend/src/controllers/SessionController.js
@@ -18,9 +18,19 @@ module.exports = {
       const email = req.body.email
     // Forma equivalente de escrever essa linha (Desestruturação)
     // const {email} = req.body;
-    // await faz com que aguarde o usuario ser criado caso demore
-    const user = await User.create({email});
-    // Deverá retornar os dados de cadastro dentro do banco de dados
+
+// await faz com que aguarde o usuario ser criado/validade no banco caso demore
+
+  //Para o caso do usuario ja existir no BCD
+  //Existem varias formas de buscar o user, vamos buscar pelo email
+  // pois é so o que temos
+  // Se encontrar o usurio vai armazenar na variavel user
+    let user = await User.findOne({email:email});
+    //Se o usuario não for encontrado no bcd
+    if(!user){
+        user = await User.create({email});
+        // Deverá retornar os dados de cadastro dentro do banco de dados
+    }
     return res.json(user);
   } 
 };
