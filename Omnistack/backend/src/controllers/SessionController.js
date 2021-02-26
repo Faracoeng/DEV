6a819142b44cdef299016fb2f484028c217d04c1
Faracoeng <alfmazucheli@gmail.com>
2021-02-26 09:31:07 -0300

    Iniciando controle de sessão

---
+++ Omnistack/backend/src/controllers/SessionController.js
@@ -0,0 +1,11 @@
+// Métofdos:
+// index: Retorna uma listagem de sessoes
+// show: Retorna uma unica sessão
+// store: Criar uma sessao
+// update: Atualizar uma sessão
+// destroy: Remover/Deletar uma sessão
+
+// exportar um objeto
+module.exports = {
+    
+};
