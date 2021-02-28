ade178565d10abf3d34e83e5d8efc601fb6a027e
Faracoeng <alfmazucheli@gmail.com>
2021-02-28 11:47:15 -0300

    Ajustes de CSS

--- Omnistack/frontend/src/App.js
+++ Omnistack/frontend/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react'; //SEmpre tem que importar React
 import './App.css'; // Impota um arq.cc e automaticamente integra CSS com HTML
 
+// Importando a logo
+import logo from './assets/logo.svg';
+
 
 // Componente é uma função em JS que retorna um HTML
 
 // Este componente se chama APP() que é importado em index.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <img src={logo} alt="Valhalla"/>
+
+        
     </div>
   );
 }
