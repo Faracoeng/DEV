d64093b3d47888e65124bc0e060688bd593de55f
Faracoeng <alfmazucheli@gmail.com>
2021-03-05 08:17:26 -0300

    Carregamento de listagem ok

--- Omnistack/frontend/src/paginas/Dashboard/index.js
+++ Omnistack/frontend/src/paginas/Dashboard/index.js
@@ -1,6 +1,26 @@
 // Criar um componente REACT vazio
-import React from 'react'; 
+// Assim que usuário acessar a página, necessário carregar informações
+// para isso utilizar {useEffect}
+import React, {useEffect} from 'react'; 
+//Iporar api
+import api from '../../services/api';
 
 export default function Dashboard(){
+
+    useEffect(() => {
+        async function carregarListagem(){
+            //pegar id do usuario do bcd do navegador
+            const idUsuario = localStorage.getItem('user');
+            // Fazendo um get pra pegar a listagem de spots dessa rota,
+            //e filtrar o id do usuário que se deseja carregar a listagem,
+            // enviando como cabeçalho seu id salvo no  navegador
+            const resposta = await api.get('/dashboard',{
+                headers: {idUsuario}
+            });
+            console.log(resposta.data); 
+        }
+        carregarListagem();
+    },[]);
+
     return <h1>Dashboard</h1>
 }
