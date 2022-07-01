9edde861c3179efdc333e2248377ed8866b9d717
Faracoeng <alfmazucheli@gmail.com>
2022-07-01 10:23:24 -0300

    log message implementado

--- API-node-docker/Curso-Docker/build-dev/run.py
+++ API-node-docker/Curso-Docker/build-dev/run.py
@@ -1,5 +1,12 @@
 import logging
-import http.server
+import http.server as server
 import socketserver
 import getpass
 
+class MyHTTPHandler(server.SimpleHTTPRequestHandler):
+    def  log_message(self, format, *args) -> None:
+        logging.info("%s - - [%s] %s\n"% (
+            self.client_address[0],
+            self.log_date_time_string(),
+            format%args
+        ))
