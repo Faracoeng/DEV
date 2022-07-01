9acab879cafacaf1365fa88098b79c4b7009d37e
Faracoeng <alfmazucheli@gmail.com>
2022-07-01 10:27:06 -0300

    python server implementado

--- API-node-docker/Curso-Docker/build-dev/run.py
+++ API-node-docker/Curso-Docker/build-dev/run.py
@@ -4,9 +4,26 @@ import socketserver
 import getpass
 
 class MyHTTPHandler(server.SimpleHTTPRequestHandler):
+    # Sobreescrevendo o método
     def  log_message(self, format, *args) -> None:
         logging.info("%s - - [%s] %s\n"% (
             self.client_address[0],
             self.log_date_time_string(),
             format%args
         ))
+
+logging.basicConfig(
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('inicializando...')
+PORT = 8000
+
+#Criando servidor TCP
+
+httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
+logging.info('escutando a porta: %s', PORT)
+logging.info('usuário: %s', getpass.getuser())
+httpd.serve_forever()
