cb3304cb0a67e446816773dd2e881e5ff50c66ab
Faracoeng <alfmazucheli@gmail.com>
2022-08-10 20:54:01 -0300

    Iniciando implementação do backend

---
+++ API-node-docker/Curso-Docker/CadastroSimples-Docker_Compose/node-mongo-compose/backend/app.js
@@ -0,0 +1,10 @@
+//Importantado servidor WEB express
+
+const express = require('express')
+// Para implementar o webservice
+const restful = require('node-restful')
+//Construtor do express
+const server = express()
+// Mongoose disponivel a partir do node restful
+const Mongoose = restful.Mongoose
+
