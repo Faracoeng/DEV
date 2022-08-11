2aca049759ecfeddbf081d6ebe81f1497dc36ec7
Faracoeng <alfmazucheli@gmail.com>
2022-08-11 15:13:32 -0300

    API e Rota implementada

--- API-node-docker/Curso-Docker/CadastroSimples-Docker_Compose/node-mongo-compose/backend/app.js
+++ API-node-docker/Curso-Docker/CadastroSimples-Docker_Compose/node-mongo-compose/backend/app.js
@@ -7,8 +7,9 @@ const restful = require('node-restful')
 const server = express()
 // Mongoose disponivel a partir do node restful
 const mongoose = restful.Mongoose
+const bodyParser = require('body-parser')
 
-
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
@@ -17,7 +18,31 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 //Teste get na raiz retorna a string 'Backend'
-server.length('/', (req, res, next) => res.send('Backend'))
+server.get('/', (req, res, next) => res.send('Backend'))
+
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// Mapeamento do objeto documento (ODM)
+const Client = restful.model('Client', {
+    
+    name: {type: String, required: true}
+
+})
+
+
+// API REST
+
+// Isso vem do NodeRestful
+Client.methods(['get','post','put','delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Rotas da API
+
+Client.register(server, '/clients')
+
 
 //START SERVER
 server.listen(3000)
